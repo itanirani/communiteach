cb52186652a847033a3b05562613158d0e2a8f62
Rani Itani <itani.rani@gmail.com>
2014-09-27 11:33:04 -0400

    added pledge model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927143219) do
+ActiveRecord::Schema.define(version: 20140927152007) do
 
   create_table "contributors", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pledges", force: true do |t|
+    t.integer  "amount"
+    t.integer  "project_id"
+    t.integer  "contributor_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
