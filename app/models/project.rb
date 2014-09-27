9931684823259dbad29f7f2b4f8c1fcda1a8f08a
Kenneth <kennethmak@Kenneths-MacBook-Pro.local>
2014-09-27 15:14:32 -0400

    changes to code for paperclip

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
 	belongs_to :staff
 	has_many :contributors though: :pledges
+	
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
