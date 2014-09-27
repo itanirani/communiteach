2d7263e249ca2fd69aaaf4e777cd866255f75245
Kenneth <kennethmak@Kenneths-MacBook-Pro.local>
2014-09-27 15:07:25 -0400

    installed paperclip, will add functionality to projects controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ActiveRecord::Base
 	belongs_to :staff
 	has_many :contributors though: :pledges
-
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+	do_not_validate_attachment_file_type :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927152007) do
+ActiveRecord::Schema.define(version: 20140927184305) do
 
   create_table "contributors", force: true do |t|
     t.string   "first_name"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 20140927152007) do
     t.integer  "staff_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "schools", force: true do |t|
